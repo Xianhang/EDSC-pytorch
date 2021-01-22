ee1ab580898cef02a348cc1bc85ceee484f2dc6f
Xianhang <1183293632@qq.com>
2021-01-22 11:19:40 +0800

    Update EDSC.py in an efficient manner

--- networks/EDSC.py
+++ networks/EDSC.py
@@ -11,7 +11,7 @@ import torchvision.models as models
 import math
 import sys
 import torch.nn.functional as F
-from networks import sepconv
+from networks import dsepconv
 
 
 
@@ -165,121 +165,6 @@ class Network(nn.Module):
         grad_output = (grad_output[i] * 0.1 for i in range(len(grad_output)))
 
     def forward(self, tensors):
-        def AdaptiveDC(TensorInput, Offsetx, Offsety, Mask):
-            '''
-            Adaptive Deformable Convolution
-            :param TensorInput: inputframe
-            :param Offsetx: offset x
-            :param Offsety: offset y
-            :param Mask: mask for each pixel
-            :return: bilnear interpolated point of a pixel, a ks times bitter image
-            '''
-
-            def _get_p_n(kernel_size, N, dtype):
-                p_n_x, p_n_y = torch.meshgrid(
-                    torch.arange(-(kernel_size - 1) // 2, (kernel_size - 1) // 2 + 1),
-                    torch.arange(-(kernel_size - 1) // 2, (kernel_size - 1) // 2 + 1))
-
-                p_n = torch.cat([torch.flatten(p_n_x), torch.flatten(p_n_y)], 0)
-                # p_n = [-1, -1, -1,  0,  0,  0,  1,  1,  1, -1,  0,  1, -1,  0,  1, -1,  0,  1]
-                p_n = p_n.contiguous().view(1, 2 * N, 1, 1).type(dtype)
-                # changed into a 4 dimensional tensor, which is returned in this function
-
-                return p_n
-
-            def _get_p_0(stride, h, w, N, dtype):
-                p_0_x, p_0_y = torch.meshgrid(
-                    torch.arange(1, h * stride + 1, stride),
-                    torch.arange(1, w * stride + 1, stride))
-
-                p_0_x = torch.flatten(p_0_x).contiguous().view(1, 1, h, w).repeat(1, N, 1, 1)
-                p_0_y = torch.flatten(p_0_y).contiguous().view(1, 1, h, w).repeat(1, N, 1, 1)
-                #  shape = (Batch, N, h, w)
-                p_0 = torch.cat([p_0_x, p_0_y], 1).type(dtype)
-
-                return p_0  # shape = (Batch, 2*N, h, w)
-
-            def _get_p(offset, dtype):
-                N, h, w = offset.size(1) // 2, offset.size(2), offset.size(3)
-
-                # (1, 2N, 1, 1)
-                p_n = _get_p_n(int(math.sqrt(N)), N, dtype)
-                # (1, 2N, h, w)
-                p_0 = _get_p_0(1, h, w, N, dtype)
-                p = p_0 + p_n + offset
-                return p
-
-            def _get_x_q(x, q, N):
-                b, h, w, _ = q.size()
-                padded_w = x.size(3)
-                c = x.size(1)
-                # (b, c, h*w)
-                x = x.contiguous().view(b, c, -1)
-
-                # (b, h, w, N)
-                index = q[..., :N] * padded_w + q[...,
-                                                N:]  # offset_x*w + offset_y, due to the same operation of each channel, the shape of index has no channel dimension
-                # (b, c, h*w*N)
-                index = index.contiguous().unsqueeze(dim=1).expand(-1, c, -1, -1, -1).contiguous().view(b, c, -1)
-
-                x_offset = x.gather(dim=-1, index=index).contiguous().view(b, c, h, w, N)
-
-                return x_offset
-
-            def _reshape_x_offset(x_offset, ks):
-                b, c, h, w, N = x_offset.size()
-                x_offset = torch.cat(
-                    [x_offset[..., s:s + ks].contiguous().view(b, c, h, w * ks) for s in range(0, N, ks)], dim=-1)
-                x_offset = x_offset.contiguous().view(b, c, h * ks, w * ks)
-
-                return x_offset
-
-            N = Offsetx.size(1)
-            ks = int(math.sqrt(N))
-            p = _get_p(torch.cat([Offsetx, Offsety], 1), Offsetx.data.type())
-            # (b, h, w, 2N)
-            p = p.contiguous().permute(0, 2, 3, 1)
-
-            q_lt = p.detach().floor()  # both of the x,y coordinate should be floored
-            q_rb = q_lt + 1
-
-            q_lt = torch.cat([torch.clamp(q_lt[..., :N], 0, TensorInput.size(2) - 1),
-                              torch.clamp(q_lt[..., N:], 0, TensorInput.size(3) - 1)],
-                             dim=-1).long()  # keep q in in the frame.
-            q_rb = torch.cat([torch.clamp(q_rb[..., :N], 0, TensorInput.size(2) - 1),
-                              torch.clamp(q_rb[..., N:], 0, TensorInput.size(3) - 1)], dim=-1).long()
-            q_lb = torch.cat([q_lt[..., :N], q_rb[..., N:]], dim=-1)
-            q_rt = torch.cat([q_rb[..., :N], q_lt[..., N:]], dim=-1)
-
-            # clip p
-            p = torch.cat([torch.clamp(p[..., :N], 0, TensorInput.size(2) - 1),
-                           torch.clamp(p[..., N:], 0, TensorInput.size(3) - 1)], dim=-1)  # float
-
-            # bilinear kernel (b, h, w, N)
-            g_lt = (1 + (q_lt[..., :N].type_as(p) - p[..., :N])) * (1 + (q_lt[..., N:].type_as(p) - p[..., N:]))
-            g_rb = (1 - (q_rb[..., :N].type_as(p) - p[..., :N])) * (1 - (q_rb[..., N:].type_as(p) - p[..., N:]))
-            g_lb = (1 + (q_lb[..., :N].type_as(p) - p[..., :N])) * (1 - (q_lb[..., N:].type_as(p) - p[..., N:]))
-            g_rt = (1 - (q_rt[..., :N].type_as(p) - p[..., :N])) * (1 + (q_rt[..., N:].type_as(p) - p[..., N:]))
-
-            # (b, c, h, w, N)
-            x_q_lt = _get_x_q(TensorInput, q_lt, N)
-            x_q_rb = _get_x_q(TensorInput, q_rb, N)
-            x_q_lb = _get_x_q(TensorInput, q_lb, N)
-            x_q_rt = _get_x_q(TensorInput, q_rt, N)
-
-            # (b, c, h, w, N)
-            x_offset = g_lt.unsqueeze(dim=1) * x_q_lt + g_rb.unsqueeze(dim=1) * x_q_rb + g_lb.unsqueeze(
-                dim=1) * x_q_lb + g_rt.unsqueeze(dim=1) * x_q_rt
-
-            m = Mask.contiguous().permute(0, 2, 3, 1)
-            m = m.unsqueeze(dim=1)
-            m = torch.cat([m for _ in range(x_offset.size(1))], dim=1)
-            x_offset *= m
-            x_offset = _reshape_x_offset(x_offset, ks)
-
-            return x_offset
-
-
         tensorFirst = tensors[0]
         tensorSecond = tensors[1]
         if self.isMultiple:
@@ -314,12 +199,13 @@ class Network(nn.Module):
             v2 = self.moduleVertical2(torch.cat([tensorCombine, 1. - tensorTime], 1))
             h1 = self.moduleHorizontal1(torch.cat([tensorCombine, tensorTime], 1))
             h2 = self.moduleHorizontal2(torch.cat([tensorCombine, 1. - tensorTime], 1))
-            # resampled frames guided by offsets and masks
-            tensorSampled1 = AdaptiveDC(tensorFirst, self.moduleOffset1x(torch.cat([tensorCombine, tensorTime], 1)),
-                                        self.moduleOffset1y(torch.cat([tensorCombine, tensorTime], 1)),
-                                        self.moduleMask1(torch.cat([tensorCombine, tensorTime], 1)))
-            tensorSampled2 = AdaptiveDC(tensorSecond,
-                                        self.moduleOffset2x(torch.cat([tensorCombine, 1. - tensorTime], 1)),
+
+            tensorDot1 = dsepconv.FunctionDSepconv(tensorFirst, v1, h1,
+                                                   self.moduleOffset1x(torch.cat([tensorCombine, tensorTime], 1)),
+                                                   self.moduleOffset1y(torch.cat([tensorCombine, tensorTime], 1)),
+                                                   self.moduleMask1(torch.cat([tensorCombine, tensorTime], 1)))
+            tensorDot2 = dsepconv.FunctionDSepconv(tensorSecond, v2, h2,
+                                                   self.moduleOffset2x(torch.cat([tensorCombine, 1. - tensorTime], 1)),
                                         self.moduleOffset2y(torch.cat([tensorCombine, 1. - tensorTime], 1)),
                                         self.moduleMask2(torch.cat([tensorCombine, 1. - tensorTime], 1)))
         else:
@@ -334,16 +220,8 @@ class Network(nn.Module):
             mask1 = self.moduleMask1(tensorCombine)
             mask2 = self.moduleMask2(tensorCombine)
 
-            # resampled frames guided by offsets and masks
-            tensorSampled1 = AdaptiveDC(tensorFirst, offset1x,
-                                        offset1y,
-                                        mask1)
-            tensorSampled2 = AdaptiveDC(tensorSecond, offset2x,
-                                        offset2y,
-                                        mask2)
-
-        tensorDot1 = sepconv.FunctionSepconv(tensorInput=tensorSampled1, tensorVertical=v1, tensorHorizontal=h1)
-        tensorDot2 = sepconv.FunctionSepconv(tensorInput=tensorSampled2, tensorVertical=v2, tensorHorizontal=h2)
+            tensorDot1 = dsepconv.FunctionDSepconv(tensorFirst, v1, h1, offset1x, offset1y, mask1)
+            tensorDot2 = dsepconv.FunctionDSepconv(tensorSecond, v2, h2, offset2x, offset2y, mask2)
 
         if self.useBias:
                 return tensorDot1 + tensorDot2 + self.moduleBias(tensorCombine)
